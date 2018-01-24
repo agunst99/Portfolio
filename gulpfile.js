53652264e5bd63ee853f68282182055a6474e6cf
Andrew Gunst <agunst99@gmail.com>
2018-01-24 11:22:54 -0500

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,11 +11,11 @@ See the README.md for gulp commands.
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-// var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var reload = browserSync.reload;
-// var filter = require('gulp-filter');
-// var uglify = require('gulp-uglify');
-//var rename = require("gulp-rename");
+var filter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
 
 gulp.task('serve', function () {
     // Serve files from the root of the project
@@ -28,33 +28,33 @@ gulp.task('serve', function () {
 });
 
 // Compiles SCSS files from /scss into /css
-// gulp.task('sass', function() {
-//     return gulp.src('scss/main.scss')
-//       .pipe(sass())     
-//       .pipe(gulp.dest('css'))
-//       .pipe(browserSync.reload({
-//         stream: true
-//       }))
-//   });
+gulp.task('sass', function() {
+    return gulp.src('scss/main.scss')
+      .pipe(sass())     
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
 
 // // Minifies js.
-// gulp.task('minify-js', function() {
-//     return gulp.src('js/main.js')
-//       .pipe(uglify())
-//       .pipe(rename({
-//         suffix: '.min'
-//       }))
-//       .pipe(gulp.dest('js'))
-//       .pipe(browserSync.reload({
-//         stream: true
-//       }))
-//   });
+gulp.task('minify-js', function() {
+    return gulp.src('js/main.js')
+      .pipe(uglify())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('js'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
 
 //   //Runs all appropriate tasks and watches for changes. 
-// gulp.task('dev', ['serve', 'sass', 'minify-js'], function() {
-//     gulp.watch('scss/*.scss', ['sass']);
-//     gulp.watch('*.html');
-//     gulp.watch('js/*.js', ['minify-js']);
-//     gulp.watch('*.html', reload);
-//     gulp.watch('js/**/*.js', reload);
-//   });
+gulp.task('dev', ['serve', 'sass', 'minify-js'], function() {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('*.html');
+    gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('*.html', reload);
+    gulp.watch('js/**/*.js', reload);
+  });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 /* General jQuery & JS stuff goes here. */
+jQuery(document).ready(function ($) {
+
+  
+  new WOW().init();
+})
+
+
+             
